8cfcb06a3eeb631bec1464d00ddcda8378bb0a81
git4novel <novelsylhet@gmail.com>
2023-05-03 17:48:13 +0600

    after context api adding

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
 /* eslint-disable react/jsx-no-undef */
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // import App from './App.jsx'
-import './index.css'
-import router from './routes/router'
-import { RouterProvider } from 'react-router-dom'
+import "./index.css";
+import router from "./routes/router";
+import { RouterProvider } from "react-router-dom";
+import AuthProvider from "./providers/AuthProvider";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </React.StrictMode>
+);
--- src/pages/Login/Register/Register.jsx
+++ src/pages/Login/Register/Register.jsx
@@ -1,38 +1,75 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Button, Container, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../../providers/AuthProvider";
+import { FaBeer, FaGithub } from 'react-icons/fa';
+import { FcGoogle } from 'react-icons/fc';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const Register = () => {
+  const { createUser } = useContext(AuthContext);
+
+  const handleRegister = (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+    const email = form.email?.value;
+    const name = form.name.value;
+    const password = form.password.value;
+    const photoUrl = form.photo.value;
+    console.log(email, password, photoUrl, name);
+
+    createUser(email, password).then((res) => res.json);
+  };
+
   return (
-    <Container className="">
-      <Form className="w-100 w-lg-50 border rounded p-2 mx-auto mt-2 mt-lg-4 pt-2 pt-lg-4 pb-5 mb-4">
+    <Container className="w-75 w-lg-50">
+      <Form
+        onSubmit={handleRegister}
+        className="border rounded p-2 mx-auto mt-2 mt-lg-4 pt-2 pt-lg-4 pb-5 mb-4"
+      >
         <Form.Group className="mb-3" controlId="formBasicName">
           <Form.Label>Your Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter Your Name" />
+          <Form.Control name="name" type="text" placeholder="Enter Your Name" required/>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control name="email" type="email" placeholder="Enter email" required />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            name="password"
+            type="password"
+            placeholder="Password"
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPhotoURL">
           <Form.Label>Your Photo URL</Form.Label>
-          <Form.Control type="text" placeholder="Your Photo URL" />
+          <Form.Control name="photo" type="text" placeholder="Your Photo URL" />
         </Form.Group>
-        
+
         <Button
           style={{ backgroundColor: "#D54215" }}
-          className="border mt-4"
+          className="border mt-4 btn-custom"
           type="submit"
         >
           Submit
         </Button>
-        <p>Already Have an Account? Go <Link to='/login' className="text-decoration-none">Login!</Link></p>
+        <div className="mt-3 mb-1">
+          <Button className="w-100 w-lg-50" variant="outline-primary"><FcGoogle /> Continue With Google</Button>
+          <Button className="mt-1 mt-lg-4 w-100 w-lg-50" variant="outline-secondary"><FaGithub /> Continue With Github</Button>
+        </div>
+       
+        <p>
+          Already Have an Account? Go{" "}
+          <Link to="/login" className="text-decoration-none">
+            Login!
+          </Link>
+        </p>
       </Form>
     </Container>
   );
